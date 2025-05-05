634ea04dc90b956ecb3666b5029f0eb51ccbd24e
Akshuu1 <akshata.14feb@gmail.com>
2025-05-06 00:08:31 +0530

    cheack

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const PropertyCard = ({properties}) => {
-
-
+const PropertyCard = () => {
   return (
-    <div>
-      {properties.length === 0 ? ()}
+    <div className='text-black w-[65%]'>
+      <div className='flex gap-[2vw]'>
+        <div className='w-[15vw] h-[19vw] bg-black ' ></div>
+        <div className='w-[15vw] h-[19vw] bg-black ' ></div>
+        <div className='w-[15vw] h-[19vw] bg-black ' ></div>
+      </div>
+      <div className='flex gap-[2vw]'>
+        <div className='w-[15vw] h-[19vw] bg-black ' ></div>
+        <div className='w-[15vw] h-[19vw] bg-black ' ></div>
+        <div className='w-[15vw] h-[19vw] bg-black ' ></div>
+      </div>
     </div>
   )
 }
